d0764c858930c1be9ef3292a9dd7b04255a525a8
Sagar poudel <61259271+sagarpdl000@users.noreply.github.com>
2023-09-10 03:34:08 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // Error!!! because we are using nodeJS, not browser.
+let age = 18;
+console.log(3
+    +
+    3)  // This is wrong practice, code readability should be high.
+
+// 1. Datatypes
+
+// number => 2 ti power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//2.  Object
+
+console.log(typeof age);
+console.log(typeof undefined);  // undefined 
+console.log(typeof null);       // object
+
+
