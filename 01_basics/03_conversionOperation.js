c5cef2b0c88b35dc40dfddbd19eb2e1b9cf0457d
Sagar poudel <61259271+sagarpdl000@users.noreply.github.com>
2023-09-11 03:16:03 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,11 +18,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let isNumber = 33;
 let stringConversion = String(isNumber);
-console.log(stringConversion)
-console.log(typeof stringConversion)
+// console.log(stringConversion)
+// console.log(typeof stringConversion)
 
 
+// ====================== Operations ==============
 
+let value = 3;
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+
+let str1 = "Hello";
+let str2 = " javaScript!";
+let str3 = str1 + str2;
+// console.log(str3)
+
+// ===================
+
+// console.log ("1" +2);
+// console.log (1 +"2");
+// console.log ("1" +2 + 2);
+// console.log (1 +2 + "2");
+// If the string is the first value then, every value is treated as string and if the string is at last then the operation at first will done first.
+// All these things are confusing so make sure you know what you are doing.
+
+
+let gameCounter = 100;
+// gameCounter++;   =>101
+++ gameCounter;   
+console.log(gameCounter)
 
 
 
