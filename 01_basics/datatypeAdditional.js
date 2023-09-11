56ee6c6fe95f7e4e73eca88bb88ee03c701caed5
Sagar poudel <61259271+sagarpdl000@users.noreply.github.com>
2023-09-11 06:50:12 +0000

    Stack and Heap example

--- 01_basics/datatypeAdditional.js
+++ 01_basics/datatypeAdditional.js
@@ -3,7 +3,7 @@
 
 const id = Symbol('123')
 const anotherid = Symbol('123')
-// console.log(id == anotherid)
+console.log(id == anotherid)
 
 
 
@@ -23,4 +23,29 @@ const myFunction = function(){
 }
 
 
-console.log(typeof myFunction)
+// console.log(typeof myFunction)
+
+// =======================Stack and heap memory============
+
+// Stack (primitive) ,Heap (Non-primitive)
+
+let myRealName = "Sagar"
+let myName = myRealName; //This gets the copy of myRealName not the original value.
+myName = "hero";
+
+// console.log(myRealName)
+// console.log(myName)
+
+let user1 = {
+    name: "Sagar",
+    email: "Sagar123@gmail.com"
+}
+
+let user2 = user1;
+
+user2.email = "Pdlsgr62@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
+
+// Since this is an object which is stored in heap, when we assign the value of user2 as user1, it will assign the original value not the copy so, when we change the value of email in user2, the value of email in user1 is also changed.
