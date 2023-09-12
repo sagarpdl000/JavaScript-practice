6f805eb68bff89353c39eb7ab5a745d1dfc3dd94
Sagar poudel <61259271+sagarpdl000@users.noreply.github.com>
2023-09-12 04:44:01 +0000

    Numbers

---
+++ 01_basics/05_numbers_maths.js
@@ -0,0 +1,22 @@
+const score = 99.99;
+// console.log(score);
+
+const newScore = new Number(99.5);
+// console.log(newScore)
+
+// const newType = newScore.toString()
+// console.log(typeof newType)
+// console.log(newType.length)
+
+console.log(newScore.toFixed(2))
+
+const otherNumber = 724.2252;
+console.log(otherNumber.toPrecision(5))
+
+const hundreds = 1000000000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ================= Maths ============================
+
+
