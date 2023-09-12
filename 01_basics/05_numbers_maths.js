ee83051d297fd89fac04d3a7b0986ecbe5d3cc5d
Sagar poudel <61259271+sagarpdl000@users.noreply.github.com>
2023-09-12 05:20:13 +0000

    Maths

--- 01_basics/05_numbers_maths.js
+++ 01_basics/05_numbers_maths.js
@@ -8,15 +8,38 @@ const newScore = new Number(99.5);
 // console.log(typeof newType)
 // console.log(newType.length)
 
-console.log(newScore.toFixed(2))
+// console.log(newScore.toFixed(2))
 
 const otherNumber = 724.2252;
-console.log(otherNumber.toPrecision(5))
+// console.log(otherNumber.toPrecision(5))
 
-const hundreds = 1000000000000
-console.log(hundreds.toLocaleString('en-IN'))
+const hundreds = 1000000000000;
+// console.log(hundreds.toLocaleString('en-IN'))
 
 
 // ================= Maths ============================
 
 
+// console.log(Math);
+// console.log(Math.abs(-4));  //It changes the negative number to a positive number. (Note: It doesn't change from positive to negative)
+
+// console.log(Math.round(98.25))
+// console.log(Math.ceil(98.25))  
+// console.log(Math.floor(98.25))  
+
+// console.log(Math.min(98, 100, 5, -10))  
+// console.log(Math.max(98, 100, 5, -10)) 
+
+
+
+// console.log(Math.random())  // Gives the random value
+// console.log((Math.random()*10) + 1) //It will help to avoid getting 0.
+
+// console.log(Math.floor((Math.random()*10) + 1))
+
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1) + min))
+
+console.log(Math.floor(Math.random() * (max - min + 1)+ min));
