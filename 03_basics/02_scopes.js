9f823dcf6e951c0e2b223bcc745444578ae7fc72
Sagar poudel <61259271+sagarpdl000@users.noreply.github.com>
2023-09-16 07:57:21 +0000

    Nested scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,7 +8,7 @@ var c = 40  //Global scope   It can be available inside block.
 
 if(true){
     let a = 20;      //Block scope     It shouldn't be available outside block.
-    console.log("Inner: ",a)    // This a will give the value of the a which is defined inside the scope only.
+   // console.log("Inner: ",a)    // This a will give the value of the a which is defined inside the scope only.
     const b = 30;       //Block scope
     // var c = 40;         //Block scope 
     
@@ -16,10 +16,30 @@ if(true){
 
 }
 
-console.log("Outer: ",a)  //This "a" will give the value of "a" which is defined outside the scope only.
+// console.log("Outer: ",a)  //This "a" will give the value of "a" which is defined outside the scope only.
 
 
 // console.log(a)
 // console.log(b)
 //console.log(c)  
+
+
+
+//==========****Nested scope==========****
+
+function one(){
+    const firstName = "Aubrey"
+    
+    function two(){
+        const middleName = "Drake"
+        console.log(firstName);
+    }
+    //console.log(middleName); This is outside the scope of the function Two() so this is not allowded.
+    two();
+}
+
+one()
+
+
+
 
