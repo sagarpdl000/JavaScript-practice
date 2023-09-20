26a703bfb634d6a02402001dfbb41f06cec336d4
Sagar poudel <61259271+sagarpdl000@users.noreply.github.com>
2023-09-20 03:04:19 +0000

    If else statement

---
+++ 04_control_flow/01_controlFlow.js
@@ -0,0 +1,40 @@
+//if
+
+if (true){
+    // This code inside the scope will be executed.
+}
+
+if (false){
+    // This code inside the scope will not be executed.
+}
+
+// if (2==2){
+//     console.log("Hello javascript!")
+// }
+
+// <, >, <=, >=, ==, !=, === (It also check the type), !==
+
+
+
+
+
+// const temperature = 15;
+// if (temperature <=15){
+//     console.log("It is cold today.")
+// }
+
+// else{
+//     console.log("It is hot today.")
+// }
+
+
+// Implicit scope
+
+const balance  = 3000000;
+
+//if (balance > 2500000) console.log("That's great.")
+
+
+
+
+
