78ee2eec9a76a8d12b68b29ff256d18246e86988
Sagar poudel <61259271+sagarpdl000@users.noreply.github.com>
2023-09-21 05:04:34 +0000

    While and do while loop

---
+++ 05_iterations/02_while_doWhile.js
@@ -0,0 +1,26 @@
+// While loop
+let i = 0;
+while (i<=10) {
+    //console.log(`Value if i is ${i}`);
+    i++;
+}
+
+// While loop on array
+
+let myArray = ["sagar", "pdl", "bhaktapur"]
+i = 0;
+while (i < myArray.length) {
+    //console.log(myArray[i])
+    i++
+}
+
+
+// Do while loop
+
+let score = 1;
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
+
+
